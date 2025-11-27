96aae8ffa2202657c0708e0e5f178262fbc0b05c
Fáber Sándor <sandorwow@gmail.com>
2025-11-27 16:26:38 +0100

    Delete download-gradle-wrapper.sh

--- download-gradle-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "📥 Gradle wrapper jar letöltése..."
-
-# Gradle wrapper jar letöltése
-curl -L -o gradle/wrapper/gradle-wrapper.jar \
-    https://github.com/gradle/gradle/raw/v7.5/gradle/wrapper/gradle-wrapper.jar
-
-echo "✅ Gradle wrapper.jar letöltve!"
-echo "🎯 Most futtasd: chmod +x gradlew"
